6f5b0cd56641e08710b0bab917bd4daad093eeca
mohabtarig2 <74310555+mohabtarig2@users.noreply.github.com>
2021-08-06 22:35:16 +0400

    third commit

--- resources/views/document.blade.php
+++ resources/views/document.blade.php
@@ -1,1 +1,28 @@
 document
+<style>
+    p{
+        line-height: 1cm;
+        font-size:
+    }
+    h1{
+        color: #dbb38f ;
+    }
+        </style>
+    <div class="container mt-5">
+
+    <div class="row text-right" dir="rtl">
+        <div class="col-md-6 col-sm-12 mt-5">
+            <h1> خدمات تخليص المعاملات</h1>
+            <p>تخليص جميع المعاملات الحكومية</p>
+            <p>تخليص معاملات وزارة العمل والعمال (تسهيل)</p>
+            <p> تخليص معاملات الجوازات (أمر)</p>
+            <p>إصدار الإقامات العائلية</p>
+            <p> تخليص المعاملات بالمحاكم والنيابات</p>
+        </div>
+    <div class="col-md-6 col-sm-12">
+        <img  src="{{URL::asset('/image/passport.svg')}}" width="90%"/>
+    </div>
+    </div>
+
+
+
